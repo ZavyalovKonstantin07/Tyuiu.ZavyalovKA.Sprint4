110ed74bc8701c547419ab29f856cad6e58301ba
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-02 14:48:17 +0500

    Add project files.

---
+++ Tyuiu.ZavyalovKA.Sprint4.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ZavyalovKA.Sprint4.Task0.V13.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint4.Task0.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumEvenArrEl()
+        {
+            DataService ds = new DataService();
+            int[] array = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+            int res = ds.GetSumEvenArrEl(array);
+            int wait = 26;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint4.Task0.V13/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint4.Task0.V13.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+int[] array = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+Console.WriteLine("Исходный массив: ");
+for (int i =0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int sum = ds.GetSumEvenArrEl(array);
+
+Console.WriteLine("Сумма четных элементов массива: " + sum);
+Console.ReadKey();
+   
+
