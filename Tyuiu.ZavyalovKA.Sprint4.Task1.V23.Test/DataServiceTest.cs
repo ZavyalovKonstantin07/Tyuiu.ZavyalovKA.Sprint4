a682f5eade5a193a2b66d369d01941b2d472b621
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-02 15:22:09 +0500

    Task1

---
+++ Tyuiu.ZavyalovKA.Sprint4.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ZavyalovKA.Sprint4.Task1.V23.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint4.Task1.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[] array = { 9, 9, 5, 6, 5, 4, 4, 5, 8, 7, 6, 8, 7, 5, 9, 9, 9 };
+            int res = ds.Calculate(array);
+            int wait = 36;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint4.Task1.V23/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint4.Task1.V23.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+int len;
+Console.WriteLine("Введите количество элементов массива: ");
+len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[len];
+for (int i = 0; i < len; i++)
+{
+    Console.WriteLine("Введите значение " + i + " элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+for (int i = 0; i < len; i++)
+{
+    Console.Write(array[i] + "\t");
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int res = ds.Calculate(array);
+Console.WriteLine("Сумма четных элементов массива: " + res);
+Console.ReadKey();
