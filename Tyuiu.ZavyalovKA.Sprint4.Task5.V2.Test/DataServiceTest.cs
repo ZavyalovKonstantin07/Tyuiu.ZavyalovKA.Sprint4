3fd011de1d857d8b32d7db902e6403c6b4d23076
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-10 13:35:27 +0500

    Task5

---
+++ Tyuiu.ZavyalovKA.Sprint4.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ZavyalovKA.Sprint4.Task5.V2.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint4.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = { { -1, 2, 3 }, { -2, 3, 5 } };
+            int res = ds.Calculate(matrix);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint4.Task5.V2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.ZavyalovKA.Sprint4.Task5.V2.Lib;
+DataService ds = new DataService();
+Random rnd = new Random();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Введите количество строк матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
+Console.WriteLine("***************************************************************************");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = rnd.Next(-7, 5);
+    }
+}
+Console.WriteLine("\nМассив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+}
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int res = ds.Calculate(matrix);
+Console.WriteLine("Количество отрицательных элементов: " + res);
+Console.ReadKey();
